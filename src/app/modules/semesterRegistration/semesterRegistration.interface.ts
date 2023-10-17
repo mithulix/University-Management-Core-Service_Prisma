b68eab5e0c01932d2359845b3993f3fd993708ae
mithulix <mohaiminul.devs@gmail.com>
2023-10-17 21:53:04 +0600

    semester updated

--- src/app/modules/semesterRegistration/semesterRegistration.interface.ts
+++
@@ -1,9 +0,0 @@
-export type ISemesterRegistrationFilter = {
-  searchTerm?: string | undefined;
-  academicSemesterId?: string | undefined;
-};
-
-export type IEnrollCoursePayload = {
-  offeredCourseId: string;
-  offeredCourseSectionId: string;
-};
